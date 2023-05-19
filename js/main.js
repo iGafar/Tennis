371a9478ed8c795f70e1da2e0bb746d973d0cbd1
Gafar <104564468+iGafar@users.noreply.github.com>
2023-05-19 12:21:39 +0300

    finished adaptive for main page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const burger = document.getElementsByClassName("header__burger")[0];
+const firstLine = document.getElementsByClassName("header__burger-item")[0]
 const secondLine = document.getElementsByClassName("header__burger-item")[1];
 const thirdLine = document.getElementsByClassName("header__burger-item")[2];
 const menu = document.getElementsByClassName("menu__list")[0];
@@ -6,14 +7,18 @@ const social = document.getElementsByClassName("social")[0];
 
 burger.addEventListener("click", () => {
   if (menu.style.top === "0px") {
-    secondLine.style.transform = "translate(0px)";
-    thirdLine.style.transform = "translate(0px)";
-    menu.style = "top: -100vh;";
-    social.style.top = "-100vh";
+    firstLine.style.transform = "rotate(0deg) translate(0px, 0px)";
+    secondLine.style.transform = "rotate(0deg) translate(0px, 0px)"
+    secondLine.style.opacity = "1";
+    thirdLine.style.transform = "rotate(0deg) translate(0px, 0px)";
+    menu.style.top = "-100vh";
+    social.style.left = "-100px";
   } else {
-    secondLine.style.transform = "translate(-5px)";
-    thirdLine.style.transform = "translate(-10px)";
+    firstLine.style.transform = "rotate(45deg) translate(5px, 9px)";
+    secondLine.style.transform = "translate(100px)";
+    secondLine.style.opacity = "0"
+    thirdLine.style.transform = "rotate(-45deg) translate(5px, -9px)";
     menu.style.top = 0;
-    social.style.top = "inherit";
+    social.style.left = "10px";
   }
 });
